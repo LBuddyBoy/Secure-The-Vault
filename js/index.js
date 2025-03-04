0d1b7b2f88b4e2838600cb180e3bd935ec402db9
Ethan Toups <ethantoups05@gmail.com>
2025-03-04 02:02:39 -0600

    Finished up the js & tested.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("I'm alive");
+console.log("This is how I'm going to solve this assigment.");
+console.log("Step #1: Create a constant for the message because it won't ever change.");
+console.log("Step #2: Implement the functionality for the alert message & test.");
+console.log("Step #3: Create the calculations variable by variable applying the needed calculations.");
+
+// Assign a constant for the alert message.
+const alertMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// Add two numbers together
+let num1 = 8 + 4;
+// Multiply the result of num1 by 5
+let num2 = num1 * 5;
+// Divide the results of 'num2' by the result of 'num1'
+let num3 = num2 / num1;
+
+// Send the alert message to the viewer
+alert(`${alertMessage} ${num3}`);
